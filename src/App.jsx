0f9c45d36cd184a93c96a99356e5c1f69ab5cc3d
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2024-03-07 20:50:27 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { Steps, StepMessage } from "./components";
+import { useState } from "react";
+import { learnData } from "./util/learn-data";
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+  function onOpen() {
+    setIsOpen((is) => !is);
+  }
+
+  return (
+    <div>
+      <Steps
+        isOpen={isOpen}
+        step={step}
+        onPrevious={handlePrevious}
+        onNext={handleNext}
+        onOpen={onOpen}
+      />
+
+      {isOpen && (
+        <>
+          {step === 1 && (
+            <StepMessage step={1}>
+              {learnData[0].map((p) => (
+                <p>{p}</p>
+              ))}
+              <p>✌️</p>
+            </StepMessage>
+          )}
+
+          {step === 2 && (
+            <StepMessage step={2}>
+              {learnData[1].map((p) => (
+                <p>{p}</p>
+              ))}
+              <p>✌️</p>
+            </StepMessage>
+          )}
+          {step === 3 && (
+            <StepMessage step={3}>
+              {learnData[2].map((p) => (
+                <p>{p}</p>
+              ))}
+              <p>✌️</p>
+            </StepMessage>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/components.jsx
@@ -0,0 +1,72 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export function Steps({ isOpen, step, onPrevious, onNext, onOpen }) {
+  function handleLearnClick() {
+    window.scrollTo({ top: 450, behavior: "smooth" });
+  }
+
+  return (
+    <>
+      <button className="close" onClick={onOpen}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={handleLearnClick}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#7950f2" textColor="#fff" onClick={onPrevious}>
+              <span>👈</span> Previous
+            </Button>
+
+            <Button bgColor="#7950f2" textColor="#fff" onClick={onNext}>
+              Next <span>👉</span>
+              <span>🤓</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
